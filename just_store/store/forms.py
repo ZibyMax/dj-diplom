ac934f63574a00603594effe65a741176cbb4651
ZibyMax <max@rekstar-nn.ru>
2019-06-17 18:59:03 +0300

    исправил

--- just_store/store/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class LoginForm(forms.Form):
-    email = forms.CharField(max_length=50)
-    password = forms.CharField(max_length=50)
--- just_store/store/views.py
+++ just_store/store/views.py
@@ -1,22 +1,14 @@
-from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import reverse_lazy
-from django.views.generic import TemplateView, FormView
+from django.contrib.auth.views import LogoutView
+from django.views.generic import TemplateView
 
-from store.forms import LoginForm
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
 
-class StoreLoginView(FormView):
+class StoreLoginView(TemplateView):
     template_name = 'login.html'
-    form_class = LoginForm
-    success_url = reverse_lazy('index')
-
-    def form_valid(self, form):
-        print(123456)
-        return super().form_valid(form)
 
 
 class StoreLogoutView(LogoutView):
