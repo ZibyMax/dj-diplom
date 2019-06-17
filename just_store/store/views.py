051f52982102393b14b001518534ab80ea138bdb
ZibyMax <max@rekstar-nn.ru>
2019-06-17 18:57:31 +0300

    хрень

--- just_store/store/views.py
+++ just_store/store/views.py
@@ -1,13 +1,22 @@
 from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView, FormView
+
+from store.forms import LoginForm
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
 
-class StoreLoginView(LoginView):
+class StoreLoginView(FormView):
     template_name = 'login.html'
+    form_class = LoginForm
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        print(123456)
+        return super().form_valid(form)
 
 
 class StoreLogoutView(LogoutView):
