45e9c5cc1fca990747742c10bb2ee7b2f448ae43
ZibyMax <max@rekstar-nn.ru>
2019-06-17 22:32:55 +0300

    что делать с email!

--- just_store/store/views.py
+++ just_store/store/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import User
 from django.contrib.auth.views import LogoutView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.views.generic import TemplateView
-
 
 
 class IndexView(TemplateView):
@@ -9,6 +11,12 @@ class IndexView(TemplateView):
 
 class StoreLoginView(TemplateView):
     template_name = 'login.html'
+
+    def post(self, request, *args, **kwargs):
+        print(request.POST)
+        user = User.objects.filter(email=request.POST['username'])
+        print(user)
+        return HttpResponseRedirect(reverse('index'))
 
 
 class StoreLogoutView(LogoutView):
