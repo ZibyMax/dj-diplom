984b7215146b723a140664ba7c06894cc7471137
ZibyMax <max@rekstar-nn.ru>
2019-07-05 17:56:09 +0300

    бля

--- just_store/store/models.py
+++ just_store/store/models.py
@@ -1,20 +1,9 @@
 from django.db import models
 
 
-class MainSection(models.Model):
-    title = models.CharField(max_length=100, verbose_name='Основной раздел')
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        verbose_name = 'Раздел'
-        verbose_name_plural = 'Разделы'
-
-
 class Section(models.Model):
-    title = models.CharField(max_length=100, verbose_name='Дополнительный раздел')
-    parent = models.ForeignKey(MainSection, null=False, verbose_name='Родительский раздел', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, verbose_name='Раздел')
+    parent = models.ForeignKey('self', default=None, verbose_name='Родительский раздел', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
