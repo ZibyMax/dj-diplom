2e545207525c45c1e46344d6a12e9ac24c7150ea
ZibyMax <max@rekstar-nn.ru>
2019-07-10 19:23:07 +0300

    заработали кнопки

--- just_store/store/views.py
+++ just_store/store/views.py
@@ -1,5 +1,3 @@
-from itertools import count
-
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LogoutView
@@ -9,6 +7,12 @@ from django.views.generic import TemplateView, ListView, DetailView
 
 from .models import Category, Section, Product, Article
 
+cart = []
+
+
+def add_to_cart(product_id):
+    cart.append(product_id)
+    print(cart)
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -19,6 +23,12 @@ class IndexView(TemplateView):
         context['sections'] = Section.objects.all()
         context['articles'] = Article.objects.all().prefetch_related('products')
         return context
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        if 'add_to_cart' in request.POST:
+            add_to_cart(request.POST['add_to_cart'])
+        return super().get(self, request, *args, **kwargs)
 
 
 class StoreLoginView(TemplateView):
@@ -48,6 +58,7 @@ class StoreLogoutView(LogoutView):
 
 class SectionView(ListView):
     template_name = 'section.html'
+    model = Product
 
     def get_queryset(self):
         return Product.objects.filter(section=self.kwargs['pk'])
@@ -58,6 +69,11 @@ class SectionView(ListView):
         context['sections'] = Section.objects.all()
         context['current_section'] = Section.objects.get(pk=self.kwargs['pk'])
         return context
+
+    def post(self, request, *args, **kwargs):
+        if 'add_to_cart' in request.POST:
+            add_to_cart(request.POST['add_to_cart'])
+        return super().get(self, request, *args, **kwargs)
 
 
 class ProductView(DetailView):
@@ -70,15 +86,21 @@ class ProductView(DetailView):
         context['sections'] = Section.objects.all()
         return context
 
+    def post(self, request, *args, **kwargs):
+        if 'add_to_cart' in request.POST:
+            add_to_cart(request.POST['add_to_cart'])
+        return super().get(self, request, *args, **kwargs)
+
 
 class CartView(ListView):
     template_name = 'cart.html'
 
     def get_queryset(self):
-        return None
+        pass
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
         context['sections'] = Section.objects.all()
         return context
+
